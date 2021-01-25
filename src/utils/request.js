64ff2c2b5d3b989b4f59db314d690d95b3103e9d
oybek1024 <mukhiddinov.oybek1024@gmail.com>
2021-01-25 18:06:40 +0500

    request js updated

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,32 +1,25 @@
-/**
- * request 网络请求工具
- * 更详细的api文档: https://bigfish.alipay.com/doc/api#request
- */
 import { extend } from 'umi-request';
 import { notification } from 'antd';
 import router from 'umi/router';
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'Success',
+  201: 'Created',
+  202: 'Accepted',
+  204: 'No Content',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  406: 'Not Acceptable',
+  410: 'Gone',
+  422: 'Unprocessable Entity',
+  500: 'Internal Server Error',
+  502: 'Bad Gateway',
+  503: 'Service Unavailable',
+  504: 'Gateway Timeout',
 };
 
-/**
- * 异常处理程序
- */
 const errorHandler = error => {
   const { response = {} } = error;
   const errortext = codeMessage[response.status] || response.statusText;
@@ -34,9 +27,8 @@ const errorHandler = error => {
 
   if (status === 401) {
     notification.error({
-      message: '未登录或登录已过期，请重新登录。',
+      message: 'Access Denied',
     });
-    // @HACK
     /* eslint-disable no-underscore-dangle */
     window.g_app._store.dispatch({
       type: 'login/logout',
@@ -44,7 +36,7 @@ const errorHandler = error => {
     return;
   }
   notification.error({
-    message: `请求错误 ${status}: ${url}`,
+    message: `Error - ${status}: ${url}`,
     description: errortext,
   });
   // environment should not be used
@@ -61,12 +53,9 @@ const errorHandler = error => {
   }
 };
 
-/**
- * 配置request请求时的默认参数
- */
 const request = extend({
-  errorHandler, // 默认错误处理
-  credentials: 'include', // 默认请求是否带上cookie
+  errorHandler,
+  credentials: 'include',
 });
 
 export default request;
